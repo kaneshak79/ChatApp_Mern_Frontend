31e2d13cefd6fe2499caec191e97ac72fce1e198
Kanesha K <kaneshak79@gmail.com>
2026-07-02 09:45:34 +0530

    Clean up axios.js by removing commented code
    Removed commented-out code for axios instance and interceptor.

--- src/api/axios.js
+++ src/api/axios.js
@@ -1,39 +1,4 @@
-// // import axios from "axios";
 
-// // // create instance
-// // export const API = axios.create({
-// //   baseURL: "http://localhost:5000",
-// // });
-
-// // // attach token automatically
-// // API.interceptors.request.use(
-// //   (req) => {
-// //     const user = JSON.parse(localStorage.getItem("user"));
-
-// //     if (user && user.token) {
-// //       req.headers.Authorization = `Bearer ${user.token}`;
-// //     }
-
-// //     return req;
-// //   },
-// //   (error) => {
-// //     return Promise.reject(error);
-// //   }
-// // );
-
-// import axios from "axios";
-
-// export const API = axios.create({
-//   baseURL: "http://localhost:5000",
-// });
-
-// API.interceptors.request.use((req) => {
-//   const user = JSON.parse(localStorage.getItem("user"));
-//   if (user?.token) {
-//     req.headers.Authorization = `Bearer ${user.token}`;
-//   }
-//   return req;
-// });
 
 import axios from "axios";
 
